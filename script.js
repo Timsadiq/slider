a12d4ef64e70f275f3312f47afa04ef2d1303e29
timi <timilehinsadiq@gmail.com>
2024-11-04 10:43:29 -0800

    slider v1

---
+++ script.js
@@ -0,0 +1,24 @@
+let allArticles=document.getElementsByTagName('article')
+
+let index=0
+
+function showSlide(){
+    for(let each of allArticles){
+        each.style.display="none"
+    }
+    allArticles[index].style.display="block"
+}
+showSlide()
+function nextSlide(){
+    index=(index+1)%allArticles.length
+    showSlide()
+}
+function prevSlide(){
+    index=(index-1+allArticles.length)%allArticles.length
+    showSlide()
+}
+
+setInterval(function(){
+    index=(index+1)%allArticles.length
+    showSlide()
+},3000)
